9e5790a7cad68ecf5a77ce0657f21f52d316405f
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2022-08-30 17:07:19 -0400

    deploy to pages

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+var questionsArr = [
+	{
+		question: `The letter "T" is the most common in the English Language`,
+		answer: false
+	},
+	{
+		question: `Dr. Pepper is the oldest soft drink in America`,
+		answer: true
+	},
+	{
+		question: `The letter "J" is the only letter in the alphabet not included in the periodic table`,
+		answer: true
+	},
+	{
+		question: `Every country in the world has a rectangular flag`,
+		answer: false
+	},
+	{
+		question: `All the kings in a standard deck of playing cards have mustaches`,
+		answer: false
+	},
+	{
+		question: `Ears of corn have an even number of rows`,
+		answer: true
+	}
+];
+
+function runQuiz() {
+	let answerArr = [];
+
+	for (let i = 0; i < questionsArr.length; i++) {
+		let question = confirm(questionsArr[i].question);
+		let answer = questionsArr[i].answer;
+
+		if (question === answer) {
+			answerArr.push(answer);
+		}
+	}
+
+	let score = Math.round((answerArr.length / questionsArr.length) * 100);
+
+	alert(`Your score is ${score}%`);
+}
